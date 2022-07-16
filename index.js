6dc90798ac239c10a440ea8ab2864ea3224b98e1
AzureAD\JackMcKelvey <jack.mckelvey@theinsightspeople.com>
2022-07-16 14:02:27 +0100

    add outputs

--- index.js
+++ index.js
@@ -58,8 +58,13 @@ function getMovingMedian(arr) {
 
 
 console.log(getMovingMedian([3,1,3,5,10,6,4,3,1]))
+// output: [1,2,3,5,6,6,4,3]
+
 console.log(getMovingMedian([5,2,4,6]))
+// output: [2,3,4]
+
 console.log(getMovingMedian([3,0,0,-2,0,2,0,-2]))
+// output: [0,0,0,0,0,0,0]
 
 
 
